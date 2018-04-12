1e447c26cc42dcd99772b18d41a4f510933d1afe
Hzfengsy <Hzfengsy@vip.qq.com>
2018-04-12 11:22:32 +0800

    Try to build

--- semantic.bash
+++ semantic.bash
@@ -6,6 +6,6 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin cn.littleround.Main"
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Hzfengsy.Main"
 cat > program.txt   # save everything in stdin to program.txt
 $CCHK
