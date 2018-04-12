feb21ae17db5bad61181eeda7fdd07d8612b1bea
Hzfengsy <Hzfengsy@vip.qq.com>
2018-04-12 11:09:39 +0800

    Try to build

--- src/Hzfengsy/Main.java
+++ src/Hzfengsy/Main.java
@@ -44,19 +44,14 @@ public class Main {
     public static void run(String expr) throws Exception
     {
 
-        //对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
         ANTLRInputStream in = new ANTLRInputStream(expr);
 
-        //用 in 构造词法分析器 lexer，词法分析的作用是产生记号
         MLexer lexer = new MLexer(in);
 
-        //用词法分析器 lexer 构造一个记号流 tokens
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        //再使用 tokens 构造语法分析器 parser,至此已经完成词法分析和语法分析的准备工作
         MParser parser = new MParser(tokens);
 
-        //最终调用语法分析器的规则 prog，完成对表达式的验证
         ParseTree tree = parser.prog();
 //        Visitor eval = new Visitor();
 //        eval.visit(tree);
